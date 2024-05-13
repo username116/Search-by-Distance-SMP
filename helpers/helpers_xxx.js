3a3dc8f2b683a2e864fc74691e6a18850ee64663
username116 <108369600+username116@users.noreply.github.com>
2024-05-13 14:53:44 +0200

    Change again after the successful merge

--- helpers/helpers_xxx.js
+++ helpers/helpers_xxx.js
@@ -5,8 +5,8 @@
 const folders = {};
 folders.JsPackageDirs = (utils.GetPackageInfo(window.ScriptInfo.PackageId || -1) || { Directories: null }).Directories;
 if (folders.JsPackageDirs) { for (const key in folders.JsPackageDirs) { folders.JsPackageDirs[key] += '\\'; } }
-folders.xxxName = 'scripts\\SMP\\xxx-scripts\\'; // Edit here to change install path (this is relative to the profile path)
-folders.dataName = 'js_data\\';
+folders.xxxName = 'scripts\\SMP\\xxx-scripts_2\\'; // Edit here to change install path (this is relative to the profile path)
+folders.dataName = 'js_data_2\\';
 folders.xxx = folders.JsPackageDirs ? folders.JsPackageDirs.Root : fb.ProfilePath + folders.xxxName;
 folders.data = fb.ProfilePath + folders.dataName;
 folders.dataPackage = folders.JsPackageDirs ? folders.JsPackageDirs.Storage : fb.ProfilePath + folders.dataName;
